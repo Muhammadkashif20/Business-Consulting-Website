0a08545f9e4e292b77d1b25edabb8ca7a2bc1bbd
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-06-19 12:50:17 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Business-Consulting-Website
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-bold text-center m-2 p-2 text-2xl'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
