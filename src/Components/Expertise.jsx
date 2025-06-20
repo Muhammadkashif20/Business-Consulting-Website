73b544adb8c24c078286a5dd558dd6d89bdf9f45
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-06-20 16:12:05 +0500

    adding pictures in expertise component

--- src/Components/Expertise.jsx
+++ src/Components/Expertise.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import pic3 from "../Image/pic 3.png"
+import pic4 from "../Image/Rectangle 36.png"
 
 const Expertise = () => {
   return (
       <div className="mx-32 my-28">
          <div className=" flex flex-row justify-between items-center">
            <div className="flex flex-col">
-           <h1 className="font-semibold text-5xl leading-13">
+           <h1 className="font-medium text-4xl ">
              Transforming Finances <br />With Strategic Wealth <br /> Management   
            </h1>
            <div className="my-8">
@@ -23,8 +24,9 @@ const Expertise = () => {
                <button className="w-[10rem] border-2 border-gray-300 p-3   rounded-sm font-semibold cursor-pointer">Explore Services</button>
            </div>
            </div>
-          <div className="flex justify-end relative bottom-7">
-           <img src={pic3} alt="" className="w-[25rem]"/>
+          <div className="flex justify-end">
+           <img src={pic4} alt="" className="w-[25rem] "/>
+           <img src={pic3} alt="" className="w-[25rem] "/>
            </div>
          </div>
        </div>
