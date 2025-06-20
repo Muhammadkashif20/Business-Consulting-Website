69cf2fa92c3f814dfc8cc73e6e78db9088f940dc
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-06-20 15:47:52 +0500

    header completed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,26 +4,28 @@ import { FaRegEnvelope } from "react-icons/fa";
 import pic1 from "../Image/pic 1.png"
 const Header = () => {
   return (
-    <div>
-      <div className="mx-32 my-28 flex flex-col">
+    <div className="mx-32 my-28">
+      <div className=" flex flex-row justify-between items-center">
+        <div className="flex flex-col">
         <h1 className="font-semibold text-5xl leading-13">
           Navigating the <br /> <span className="text-[#008080]">Global</span> financial <br /> landscape
         </h1>
-        <div className="my-5">
+        <div className="my-8">
           <p>
             Empowering Your Journey to Global Financial <br/> Success Through
             Customized and Personalized <br/> Financial Consulting for Every Client
           </p>
         </div>
         <div className="my-10 flex gap-3">
-          <div className="bg-white p-1 w-[25%]">
+          <div className="bg-white p-1 ">
             <FaRegEnvelope className="relative top-3 left-2 text-gray-500 "/>
           <input type="text" placeholder="Your email address" className=" placeholder:text-gray-600 text-sm mx-9 relative bottom-2 outline-none"/>
           </div>
-            <button className="w-[7rem] bg-[#008080] text-white rounded-sm font-semibold">Get Started</button>
+            <button className="w-[7rem] bg-[#008080] text-white rounded-sm font-semibold cursor-pointer">Get Started</button>
         </div>
-        <div className="flex flex-row">
-        <img src={pic1} alt="" className="w-[30%]"/>
+        </div>
+       <div className="flex justify-end relative bottom-7">
+        <img src={pic1} alt="" className="w-[25rem]"/>
         </div>
       </div>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
         <div className='flex flex-row justify-around items-center m-10'>
         <h1 className='font-bold text-2xl'>Prosperix</h1>
         <ul className='flex gap-10 font-semibold'>
-          <li>Home</li>
-          <li>Services</li>
-          <li>Contact</li>
-          <li>Help</li>
+          <li className='cursor-pointer'>Home</li>
+          <li className='cursor-pointer'>Services</li>
+          <li className='cursor-pointer'>Contact</li>
+          <li className='cursor-pointer'>Help</li>
         </ul>
         <div className='flex gap-6'>
-        <button className='text-[#008080] font-semibold'>Sign In</button>
-        <button className="p-2 w-[7rem] bg-[#008080] text-white rounded-sm font-semibold">Get Started</button>
+        <button className='text-[#008080] font-semibold cursor-pointer'>Sign In</button>
+        <button className="p-2 w-[7rem] bg-[#008080] text-white rounded-sm font-semibold cursor-pointer">Get Started</button>
         </div>
       </div>
   )
