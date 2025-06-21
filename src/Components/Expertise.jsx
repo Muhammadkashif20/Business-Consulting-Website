610c76d57e2c4741d7fa4cc840a4cc35482d3f4f
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-06-21 11:54:42 +0500

    completed expertise section

--- src/Components/Expertise.jsx
+++ src/Components/Expertise.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import pic3 from "../Image/pic 3.png"
-import pic4 from "../Image/Rectangle 36.png"
+import Group95 from "../Image/Group 95.png"
+import line from "../Image/Line 10.png"
 
 const Expertise = () => {
   return (
       <div className="mx-32 my-28">
          <div className=" flex flex-row justify-between items-center">
            <div className="flex flex-col">
+            <div className='flex items-center gap-5 '>
+              <h2 className='bg-[#EDFBFA] text-[#008080] text-center w-[9rem]'>OUR EXPERTISE</h2>
+              <img src={line} alt="" className='h-[2px] w-[5rem]'/>
+            </div>
            <h1 className="font-medium text-4xl ">
              Transforming Finances <br />With Strategic Wealth <br /> Management   
            </h1>
@@ -21,12 +25,11 @@ const Expertise = () => {
              </p>
            </div>
            <div className="my-10">
-               <button className="w-[10rem] border-2 border-gray-300 p-3   rounded-sm font-semibold cursor-pointer">Explore Services</button>
+               <button className="w-[10rem] border-2 border-gray-300 p-3 rounded-sm font-semibold cursor-pointer">Explore Services</button>
            </div>
            </div>
           <div className="flex justify-end">
-           <img src={pic4} alt="" className="w-[25rem] "/>
-           <img src={pic3} alt="" className="w-[25rem] "/>
+           <img src={Group95} className="w-[27rem] h-[30rem]"/>
            </div>
          </div>
        </div>
