3e610b9e461afb0281169fdf6183e91a3273d03f
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-06-22 16:42:56 +0500

    contact page completed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,32 +3,40 @@ import Rectangle from "../Image/Rectangle 41.png";
 import line from "../Image/Line 2.png";
 import Services from "../Image/services.png";
 import { FaRegEnvelope } from "react-icons/fa";
+
 const Contact = () => {
   return (
-    <div className="mx-32 my-5">
-      <div className="relative">
-        <img src={Rectangle} alt="" className="w-[70rem] h-[25rem]" />
-        <div className="flex items-center gap-1 absolute bottom-[23rem] font-semibold text-[1rem] mx-3 mt-7">
-          <h2 className=" text-[#2CDFD5] text-center w-[9rem]">START NOW</h2>
-          <img src={line} alt="" className="h-[2px] w-[4rem]" />
-        </div>
-       <div className="flex flex-col relative bottom-[22rem] mx-10 my-5">
-        <h1 className="font-bold text-white text-3xl ">
-          Get Start For Free No <br/> Credit Card Required 
+    <div className="mx-32 my-5 relative">
+      <img src={Rectangle} alt="" className="w-[70rem] h-[25rem]" />
+      <div className="flex items-center gap-1 absolute top-6 font-semibold text-[1rem] mx-3 mt-7">
+        <h2 className="text-[#2CDFD5] text-center w-[9rem]">START NOW</h2>
+        <img src={line} alt="" className="h-[2px] w-[4rem]" />
+      </div>
+      <div className="flex flex-col absolute top-20 mx-10 my-5 w-full">
+        <h1 className="font-bold text-white text-3xl">
+          Get Start For Free No <br /> Credit Card Required
         </h1>
         <div className="mt-12 flex gap-3">
           <div className="bg-white p-1 rounded-sm">
-            <FaRegEnvelope className="relative top-3 left-2 text-gray-500 "/>
-          <input type="text" placeholder="Email Address" className=" placeholder:text-gray-600 text-sm mx-9 relative bottom-2  w-[12rem]"/>
+            <FaRegEnvelope className="relative top-3 left-2 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="placeholder:text-gray-600 text-sm mx-9 relative bottom-2 outline-none w-[12rem]"
+            />
           </div>
-            <button className="w-[7rem] bg-[#20CFC6] text-white rounded-sm font-semibold cursor-pointer">Get Started</button>
+          <button className="w-[7rem] bg-[#20CFC6] text-white rounded-sm font-semibold cursor-pointer">
+            Get Started
+          </button>
         </div>
         <div className="my-7">
-          <p className="text-[#D3D3D3]">Want to contact our team and book a call? <span className="text-[#20CFC6] cursor-pointer">Try it now</span></p>
+          <p className="text-[#D3D3D3]">
+            Want to contact our team and book a call?{" "}
+            <span className="text-[#20CFC6] cursor-pointer">Try it now</span>
+          </p>
         </div>
-        <div className="relative bottom-[21rem] flex justify-end">
-          <img src={Services} alt="" className="w-[30rem] h-[22rem]"/>
-        </div>
+        <div className="flex justify-end absolute right-[4rem] top-[-4rem]">
+          <img src={Services} alt="" className="w-[30rem] h-[22rem]" />
         </div>
       </div>
     </div>
