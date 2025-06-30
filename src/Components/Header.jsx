b2149eb9096cd0a2d95e02e712adc6c7e4571216
Muhammadkashif20 <154686104+Muhammadkashif20@users.noreply.github.com>
2025-07-01 01:47:43 +0500

    issue resolved  img width  header responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,9 +6,9 @@ import pic2 from "../Image/pic 2.png";
 const Header = () => {
   return (
     <div className="mx-32 my-16">
-      <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-col relative right-24 ">
-          <h1 className="font-semibold text-4xl leading-13">
+      <div className="flex flex-col md:flex-row lg:flex-row justify-between items-center">
+        <div className="flex flex-col">
+          <h1 className="font-semibold text-4xl lg:text-5xl  leading-13 ">
             Navigating the <br />{" "}
             <span className="text-[#008080] hover:text-[#006666] transition duration-300 ease-in-out cursor-pointer">
               Global
@@ -16,15 +16,15 @@ const Header = () => {
             financial <br /> landscape
           </h1>
 
-          <div className="my-8 text-[.85rem]">
+          <div className="my-8 text-[.85rem] lg:text-[1rem]">
             <p className="transition duration-300 ease-in-out hover:text-gray-700 cursor-pointer">
               Empowering Your Journey to Global Financial <br /> Success Through
               Customized and Personalized <br /> Financial Consulting for Every Client
-            </p>
+            </p>  
           </div>
 
-          <div className="my-4 flex flex-col gap-3 sm: flex flex-col md:flex lg:flex ">
-            <div className="bg-white p-1 rounded-sm transition duration-300 ease-in-out hover:scale-105">
+          <div className="my-4 flex flex-col gap-3 lg:flex-row ">
+            <div className="bg-white p-1 rounded-sm transition duration-300 ease-in-out hover:scale-105 h-[3rem] ">
               <FaRegEnvelope className="relative top-3 left-2 text-gray-500" />
               <input
                 type="text"
@@ -33,17 +33,18 @@ const Header = () => {
               />
             </div>
 
-            <button className="w-[20rem] h-[2rem] bg-[#008080] text-white rounded-sm font-semibold cursor-pointer transition duration-300 ease-in-out hover:bg-[#006666] hover:scale-105">
+            <button className="w-[20rem] lg:w-[8rem] h-[3rem] bg-[#008080] text-white rounded-sm font-semibold cursor-pointer transition duration-300 ease-in-out hover:bg-[#006666] hover:scale-105">
               Get Started
             </button>
           </div>
         </div>
 
-        <div className="flex justify-end relative bottom-7">
+        <div className="">
           <img
             src={pic1}
             alt=""
-            className="w-[25rem] transition duration-300 ease-in-out hover:scale-105"
+            // className="flex flex-col lg:justify-end w-[10rem] lg:w-[25rem] transition duration-300 ease-in-out hover:scale-105"
+            className=" w-[40rem] lg:w-[25rem] transition duration-300 ease-in-out hover:scale-105"
           />
         </div>
       </div>
